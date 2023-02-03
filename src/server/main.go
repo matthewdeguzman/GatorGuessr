9ad0a5b6e54d95bffc8aac5aad5f7f6c69df0db9
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2023-02-03 15:40:10 -0500

    made the createUser function for the API

--- src/server/main.go
+++ src/server/main.go
@@ -15,17 +15,17 @@ func initializeRouter() {
 
 	// Route Handlers / Endpoints
 	r.HandleFunc("/api/users", getUsers).Methods("GET")
-	r.HandleFunc("/api/users/{id}", getUser).Methods("GET")
+	r.HandleFunc("/api/users/{username}", getUser).Methods("GET")
 	r.HandleFunc("/api/users", createUser).Methods("POST")
-	r.HandleFunc("/api/users/{id}", updateUser).Methods("PUT")
-	r.HandleFunc("/api/users/{id}", deleteUser).Methods("DELETE")
+	r.HandleFunc("/api/users/{username}", updateUser).Methods("PUT")
+	r.HandleFunc("/api/users/{username}", deleteUser).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
 
 func main() {
 
-	// mock data - @TODO: Implement real database with azure
+	// mock data - @TODO - Matthew: Implement real database with azure
 	users = append(users, User{Username: "madmatt10125", Password: "good_password"})
 	users = append(users, User{Username: "thereal_throatgoat", Password: "great_password :o"})
 
--- src/server/user.go
+++ src/server/user.go
@@ -21,24 +21,34 @@ func getUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r) // gets the params
 
-	// loops through all users
+	// loops through all users encode the info if the user was found
 	for _, item := range users {
 		if item.Username == params["username"] {
 			json.NewEncoder(w).Encode(item)
 			return
 		}
 	}
+
+	// encode an empty user if the user was not found
 	json.NewEncoder(w).Encode(&User{})
 }
 
 func createUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 
+	// reads the request and puts it into user
+	var user User
+	_ = json.NewDecoder(r.Body).Decode(&user)
+
+	// appends the new user to the mock database and encodes it into w
+	users = append(users, user)
+	json.NewEncoder(w).Encode(user)
 }
 
 func updateUser(w http.ResponseWriter, r *http.Request) {
-
+	// @TODO - Ethan
 }
 
 func deleteUser(w http.ResponseWriter, r *http.Request) {
-
+	// @TODO - Ethan
 }
