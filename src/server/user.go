979c74086149b1f5d040723e46f54a6e69c09962
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2023-02-04 00:12:44 -0500

    added support for mysql database

--- src/server/user.go
+++ src/server/user.go
@@ -14,23 +14,17 @@ type User struct {
 
 func getUsers(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	var users []User
+	db.Find(&users)
 	json.NewEncoder(w).Encode(users)
 }
 
 func getUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r) // gets the params
-
-	// loops through all users encode the info if the user was found
-	for _, item := range users {
-		if item.Username == params["username"] {
-			json.NewEncoder(w).Encode(item)
-			return
-		}
-	}
-
-	// encode an empty user if the user was not found
-	json.NewEncoder(w).Encode(&User{})
+	params := mux.Vars(r)
+	var user User
+	db.First(&user, params["username"])
+	json.NewEncoder(w).Encode(user)
 }
 
 func createUser(w http.ResponseWriter, r *http.Request) {
@@ -38,10 +32,10 @@ func createUser(w http.ResponseWriter, r *http.Request) {
 
 	// reads the request and puts it into user
 	var user User
-	_ = json.NewDecoder(r.Body).Decode(&user)
+	json.NewDecoder(r.Body).Decode(&user)
 
-	// appends the new user to the mock database and encodes it into w
-	users = append(users, user)
+	// appends the new user to the database and encodes it into w
+	db.Create(&user)
 	json.NewEncoder(w).Encode(user)
 }
 
