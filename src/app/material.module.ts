1f84ab14978bdd8a6a05146bcde3b6217d7ee274
Coomesicle <stephencoomes@ufl.edu>
2023-02-06 13:36:30 -0500

    goofy solution ahhh

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import {MatInputModule} from '@angular/material/input';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 const MaterialComponents = [
     MatButtonModule,
-    MatInputModule
+    MatInputModule,
+    MatToolbarModule,
 ];
 
 @NgModule({
