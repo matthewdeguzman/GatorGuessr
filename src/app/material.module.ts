db885148775b1a5797b24167eccf3582fbb78cb6
ramawama <93228765+ramawama@users.noreply.github.com>
2023-02-06 16:05:08 -0500

    Fixed routing for different pages

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 const MaterialComponents = [
     MatButtonModule,
     MatInputModule,
     MatToolbarModule,
+    MatFormFieldModule,
 ];
 
 @NgModule({
