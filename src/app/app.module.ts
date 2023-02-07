722636601183ee0f7f3e5b0e60630014b9813a2b
ramawama <93228765+ramawama@users.noreply.github.com>
2023-02-06 19:36:46 -0500

    Successful log in page
    A clean log in ui with hidden password toggle. WooHooo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { LoginComponent } from './login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 const routes: Routes = [
   { path: '', redirectTo: 'app', pathMatch: 'full' },
@@ -25,7 +27,9 @@ const routes: Routes = [
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-    RouterModule.forRoot(routes)
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent],
