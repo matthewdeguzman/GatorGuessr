0b5b34b82d279b834ad32fccdc9c26b1dda9fb4c
Coomesicle <stephencoomes@ufl.edu>
2023-03-08 16:18:10 -0500

    Added Logo and Changed Banner

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,10 +3,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatFormFieldModule } from '@angular/material/form-field';
-
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule} from '@angular/material/icon';
 import { ReactiveFormsModule } from '@angular/forms';
+import {MatGridListModule} from '@angular/material/grid-list'; 
+import {MatMenuModule} from '@angular/material/menu';
 
 const MaterialComponents = [
     MatButtonModule,
@@ -16,6 +17,8 @@ const MaterialComponents = [
     MatCardModule,
     MatIconModule,
     ReactiveFormsModule,
+    MatGridListModule,
+    MatMenuModule
 ];
 
 @NgModule({
