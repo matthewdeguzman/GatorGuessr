5c3c3341f0efd4ce2bc1e99310ed6a289870cc60
ramawama <93228765+ramawama@users.noreply.github.com>
2023-03-28 14:29:46 -0400

    Added streetview and map grid
    Displaying streetview and map in grid format!

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -35,16 +35,40 @@ export class LandingPageComponent {
   //     });
   // }
 
+  // center: LatLngLiteral = { lat: 29.6516, lng: -82.3248 };
+
   ngOnInit(): void {
     //this.getTextFile();
     let loader = new Loader({
       apiKey: this.string,
       version: "weekly",
     });
+
     loader.load().then(() => {
+      new google.maps.StreetViewPanorama(
+        document.getElementById("Smap") as HTMLElement,
+        {
+          position: { lat: 29.652031, lng: -82.342953 },
+          pov: {
+            heading: 34,
+            pitch: 10,
+          },
+        }
+      );
       new google.maps.Map(document.getElementById("Gmap") as HTMLElement, {
-        center: { lat: 29.643946, lng: -82.355659 },
-        zoom: 6,
+        center: { lat: 29.653288, lng: -82.338712 },
+        zoom: 5,
+        fullscreenControl: false,
+        restriction: {
+          latLngBounds: {
+            //North: nw 53rd ave
+            east: -82.263414,
+            north: 29.769872,
+            south: 29.602758,
+            west: -82.420207,
+          },
+          strictBounds: false,
+        },
       });
     });
   }
