ed7e3d5c78e088c83b507d52ada65e164a217f43
ramawama <93228765+ramawama@users.noreply.github.com>
2023-03-28 11:19:05 -0400

    GOOGLE MAPS ON LANDING

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,8 +1,25 @@
+import { Loader } from "@googlemaps/js-api-loader";
 import { Component } from "@angular/core";
+//import { GoogleMapsModule } from "@angular/google-maps";
 
 @Component({
   selector: "app-landing-page",
   templateUrl: "./landing-page.component.html",
   styleUrls: ["./landing-page.component.css"],
 })
-export class LandingPageComponent {}
+export class LandingPageComponent {
+  title = "GatorMap";
+
+  ngOnInit(): void {
+    let loader = new Loader({
+      apiKey: "",
+      version: "weekly",
+    });
+    loader.load().then(() => {
+      new google.maps.Map(document.getElementById("Gmap") as HTMLElement, {
+        center: { lat: 29.643946, lng: -82.355659 },
+        zoom: 6,
+      });
+    });
+  }
+}
