87ed77b139db7f1c82f2c6395f8f83efcb2f51a5
ramawama <93228765+ramawama@users.noreply.github.com>
2023-03-28 12:40:04 -0400

    My sad sad attempts at hiding api key and what not
    I attempted to access hiden api key by reading json and text, both failed. I think it will be easier to have the backend send it to me since it will be easily accessed on the webpage if not

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,5 +1,8 @@
 import { Loader } from "@googlemaps/js-api-loader";
 import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+
 //import { GoogleMapsModule } from "@angular/google-maps";
 
 @Component({
@@ -9,10 +12,33 @@ import { Component } from "@angular/core";
 })
 export class LandingPageComponent {
   title = "GatorMap";
+  string = "";
+  // content: string;
+  // private _jsonURL = "src/assets/env.json";
+  // constructor(private http: HttpClient) {
+  //   this.getJSON().subscribe((data) => {
+  //     console.log(data);
+  //   });
+  // }
+  // public getJSON(): Observable<any> {
+  //   return this.http.get(this._jsonURL);
+  // }
+
+  // getTextFile() {
+  //   this.http
+  //     .get("/env.txt", {
+  //       responseType: "text",
+  //     })
+  //     .subscribe((data) => {
+  //       this.content = data;
+  //       //console.log(this.content);
+  //     });
+  // }
 
   ngOnInit(): void {
+    //this.getTextFile();
     let loader = new Loader({
-      apiKey: "",
+      apiKey: this.string,
       version: "weekly",
     });
     loader.load().then(() => {
