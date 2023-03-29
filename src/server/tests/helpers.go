dc4cc57e3f9c760b4a711799083f6d7f6e7af1d8
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2023-03-29 14:21:39 -0400

    📦 Added more test cases

---
+++ src/server/tests/helpers.go
@@ -0,0 +1,111 @@
+package tests
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	endpoints "github.com/matthewdeguzman/GatorGuessr/src/server/endpoints"
+	u "github.com/matthewdeguzman/GatorGuessr/src/server/structs"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func testInitMigration(t *testing.T) (db *gorm.DB) {
+	const DB_USERNAME = "cen3031"
+	const DB_NAME = "user_database"
+	const DB_HOST = "cen3031-server.mysql.database.azure.com"
+	const DB_PORT = "3306"
+
+	// Build connection string
+	DSN := DB_USERNAME + ":" + DB_PASSWORD + "@tcp" + "(" + DB_HOST + ":" + DB_PORT + ")/" + DB_NAME + "?" + "parseTime=true&loc=Local"
+
+	db, err := gorm.Open(mysql.Open(DSN), &gorm.Config{})
+	if err != nil {
+		fmt.Println(err.Error())
+		panic("Cannot connect to DB")
+	}
+
+	// migrates the server if necessary
+	db.AutoMigrate(&u.User{})
+
+	return db
+}
+
+func cleanDB(user *u.User, username string) {
+	db := testInitMigration(t)
+	db.Delete(user, "Username = ?", user.Username)
+}
+
+func mockGetUsers(w http.ResponseWriter, r *http.Request, t *testing.T) {
+	db := testInitMigration(t)
+	endpoints.GetUsers(w, r, db)
+}
+
+func mockGetUser(w http.ResponseWriter, r *http.Request, username string, t *testing.T) {
+	db := testInitMigration(t)
+
+	endpoints.SetHeader(w)
+
+	var user u.User
+	endpoints.FetchUser(db, &user, username)
+
+	if user.Username == "" {
+		endpoints.WriteErr(w, http.StatusNotFound, "")
+	}
+	endpoints.EncodeUser(user, w)
+
+}
+
+func mockCreateUser(w http.ResponseWriter, r *http.Request, user u.User, db *gorm.DB, t *testing.T) {
+	endpoints.SetHeader(w)
+
+	if endpoints.UserExists(db, user.Username) {
+		endpoints.WriteErr(w, http.StatusBadRequest, "")
+		return
+	}
+
+	hash, err := endpoints.EncodePassword(user.Password)
+
+	if err != nil {
+		endpoints.WriteErr(w, http.StatusInternalServerError, "")
+	}
+	user.Password = hash
+
+	db.Create(&user)
+	endpoints.EncodeUser(user, w)
+}
+
+func getUserTest(username string, t *testing.T) (status int) {
+	req, err := http.NewRequest("GET", "/api/users/{username}/", nil)
+	if err != nil {
+		t.Error(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		mockGetUser(w, r, username, t)
+	})
+
+	handler.ServeHTTP(rr, req)
+
+	return rr.Result().StatusCode
+}
+
+func createUserTest(user u.User, t *testing.T) (status int) {
+	db := testInitMigration(t)
+	req, err := http.NewRequest("POST", "/api/users/", nil)
+	if err != nil {
+		t.Error(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		mockCreateUser(w, r, user, db, t)
+	})
+
+	handler.ServeHTTP(rr, req)
+
+	return rr.Code
+}
--- src/server/tests/server_test.go
+++ src/server/tests/server_test.go
@@ -1,200 +1,12 @@
 package tests
 
 import (
-	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
-	endpoints "github.com/matthewdeguzman/GatorGuessr/src/server/endpoints"
 	u "github.com/matthewdeguzman/GatorGuessr/src/server/structs"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
 )
-
-func testInitMigration(t *testing.T) (db *gorm.DB) {
-	const DB_USERNAME = "cen3031"
-	const DB_NAME = "user_database"
-	const DB_HOST = "cen3031-server.mysql.database.azure.com"
-	const DB_PORT = "3306"
-
-	// Build connection string
-	DSN := DB_USERNAME + ":" + DB_PASSWORD + "@tcp" + "(" + DB_HOST + ":" + DB_PORT + ")/" + DB_NAME + "?" + "parseTime=true&loc=Local"
-
-	db, err := gorm.Open(mysql.Open(DSN), &gorm.Config{})
-	if err != nil {
-		fmt.Println(err.Error())
-		panic("Cannot connect to DB")
-	}
-
-	// migrates the server if necessary
-	db.AutoMigrate(&u.User{})
-
-	return db
-}
-
-func mockGetUsers(w http.ResponseWriter, r *http.Request, t *testing.T) {
-	db := testInitMigration(t)
-	endpoints.GetUsers(w, r, db)
-}
-
-func mockGetUser(w http.ResponseWriter, r *http.Request, username string, t *testing.T) {
-	db := testInitMigration(t)
-
-	endpoints.SetHeader(w)
-
-	var user u.User
-	endpoints.FetchUser(db, &user, username)
-
-	if user.Username == "" {
-		endpoints.UserDNErr(w)
-		return
-	}
-	endpoints.EncodeUser(user, w)
-}
-
-func getUserTest(username string, t *testing.T) (status int) {
-	req, err := http.NewRequest("GET", "/api/users/{username}", nil)
-	if err != nil {
-		t.Error(err)
-	}
-
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		mockGetUser(w, r, username, t)
-	})
-
-	handler.ServeHTTP(rr, req)
-
-	return rr.Code
-}
-
-// func getUserTest(username string, t *testing.T) string {
-// 	db := testInitMigration(t)
-// 	req, err := http.NewRequest("GET", "/api/users/{username}/", nil)
-// 	if err != nil {
-// 		t.Error(err)
-// 	}
-
-// 	mockGetUser := func(w http.ResponseWriter, r *http.Request) {
-// 		w.Header().Set("Content-Type", "application/json")
-
-// 		params := username
-// 		var user u.User
-// 		db.First(&user, "Username = ?", params)
-// 		json.NewEncoder(w).Encode(user)
-// 	}
-// 	rr := httptest.NewRecorder()
-// 	handler := http.HandlerFunc(mockGetUser)
-
-// 	handler.ServeHTTP(rr, req)
-
-// 	if status := rr.Code; status != http.StatusOK {
-// 		t.Errorf("handler return wrong status code: got %v want %v", status, http.StatusOK)
-// 	}
-
-// 	var user u.User
-// 	if err := json.Unmarshal(rr.Body.Bytes(), &user); err != nil {
-// 		t.Errorf("got invalid reponse, expected a user, got: %v", rr.Body.String())
-// 	}
-
-// 	return user.Username
-// }
-
-// func deleteUserTest(username string, t *testing.T) {
-// 	testInitMigration(t)
-// 	req, err := http.NewRequest("DELETE", "/api/users/{username}", nil)
-// 	if err != nil {
-// 		t.Error(err)
-// 	}
-
-// 	mockDeleteUser := func(w http.ResponseWriter, r *http.Request) {
-// 		w.Header().Set("Content-Type", "application/json")
-
-// 		params := username
-// 		var user u.User
-// 		db.First(&user, "Username = ?", params)
-// 		db.Delete(&user)
-// 		json.NewEncoder(w).Encode(user)
-// 	}
-// 	rr := httptest.NewRecorder()
-// 	handler := http.HandlerFunc(mockDeleteUser)
-
-// 	handler.ServeHTTP(rr, req)
-
-// 	if status := rr.Code; status != http.StatusOK {
-// 		t.Errorf("handler return wrong status code: got %v want %v", status, http.StatusOK)
-// 	}
-
-// 	var user u.User
-// 	if err := json.Unmarshal(rr.Body.Bytes(), &user); err != nil {
-// 		t.Errorf("got invalid reponse, expected a user, got: %v", rr.Body.String())
-// 	}
-// }
-
-// func createUserTest(username string, t *testing.T) {
-// 	testInitMigration(t)
-// 	req, err := http.NewRequest("POST", "/api/users", nil)
-// 	if err != nil {
-// 		t.Error(err)
-// 	}
-
-// 	mockCreateUser := func(w http.ResponseWriter, r *http.Request) {
-// 		w.Header().Set("Content-Type", "application/json")
-
-// 		var user u.User
-// 		user.Username = username
-// 		user.Password = "test-password"
-// 		db.Create(&user)
-// 		json.NewEncoder(w).Encode(user)
-// 	}
-// 	rr := httptest.NewRecorder()
-// 	handler := http.HandlerFunc(mockCreateUser)
-
-// 	handler.ServeHTTP(rr, req)
-
-// 	if status := rr.Code; status != http.StatusOK {
-// 		t.Errorf("handler return wrong status code: got %v want %v", status, http.StatusOK)
-// 	}
-
-// 	var user u.User
-// 	if err := json.Unmarshal(rr.Body.Bytes(), &user); err != nil {
-// 		t.Errorf("got invalid reponse, expected a user, got: %v", rr.Body.String())
-// 	}
-// }
-
-// func updateUserTest(username string, password string, t *testing.T) {
-// 	testInitMigration(t)
-// 	req, err := http.NewRequest("PUT", "/api/users/{username}", nil)
-// 	if err != nil {
-// 		t.Error(err)
-// 	}
-
-// 	mockUpdateUser := func(w http.ResponseWriter, r *http.Request) {
-// 		w.Header().Set("Content-Type", "application/json")
-
-// 		var user u.User
-// 		db.First(&user, "Username = ?", username)
-// 		if user.Username != "" {
-// 			user.Password = password
-// 			db.Save(&user)
-// 		}
-// 		json.NewEncoder(w).Encode(user)
-// 	}
-// 	rr := httptest.NewRecorder()
-// 	handler := http.HandlerFunc(mockUpdateUser)
-
-// 	handler.ServeHTTP(rr, req)
-
-// 	if status := rr.Code; status != http.StatusOK {
-// 		t.Errorf("handler return wrong status code: got %v want %v", status, http.StatusOK)
-// 	}
-
-// 	var user u.User
-// 	if err := json.Unmarshal(rr.Body.Bytes(), &user); err != nil {
-// 		t.Errorf("got invalid reponse, expected a user, got: %v", rr.Body.String())
-// 	}
-// }
 
 /// TESTS ///
 
@@ -233,105 +45,99 @@ func TestGetNonexiststantUser(t *testing.T) {
 }
 
 func TestCreateExistingUser(t *testing.T) {
-
+	user := u.User{
+		Username: "matthew",
+		Password: "passwordddd",
+	}
+	if status := createUserTest(user, t); status != http.StatusBadRequest {
+		t.Log(status)
+		t.Fail()
+	}
 }
 
 func TestCreateNewUser(t *testing.T) {
+	user := u.User{
+		Username: "garbledmess",
+		Password: "password!!",
+	}
 
+	if status := createUserTest(user, t); status != http.StatusOK {
+		t.Fail()
+	}
+
+	cleanDB(&user, user.Username)
+}
+
+func TestCreateUserWithoutPassword(t *testing.T) {
+	user := u.User{
+		Username: "newuseralert!!",
+	}
+
+	if status := createUserTest(user, t); status != http.StatusBadRequest {
+		t.Fail()
+	}
+
+	cleanDB(&user, user.Username)
+}
+func TestCreateWithID(t *testing.T) {
+	t.Fail()
 }
 
 func TestUpdateNonexistantUser(t *testing.T) {
-
+	t.Fail()
 }
 
 func TestUpdateExistingUser(t *testing.T) {
-
+	t.Fail()
 }
 
 func TestUpdateUserID(t *testing.T) {
-
+	t.Fail()
 }
 
 func TestUpdateUserScore(t *testing.T) {
-
+	t.Fail()
 }
 
 func TestDeleteExistingUser(t *testing.T) {
-
+	t.Fail()
 }
 
 func TestNonexistingUser(t *testing.T) {
-
+	t.Fail()
 }
 
 func TestValidateExistingUser(t *testing.T) {
-
+	t.Fail()
 }
 
 func TestValidNonexistantuser(t *testing.T) {
-
+	t.Fail()
 }
 
 func TestLeaderboardNegativeInteger(t *testing.T) {
-
+	t.Fail()
 }
 
 func TestLeaderboardFloatintPoint(t *testing.T) {
-
+	t.Fail()
 }
 
 func TestLeaderboardZero(t *testing.T) {
-
+	t.Fail()
 }
 
 func TestLeaderboardOverMaxLimit(t *testing.T) {
-
+	t.Fail()
 }
 
 func TestLeaderboardCasual(t *testing.T) {
-
+	t.Fail()
+}
+func TestPasswordEncoding(t *testing.T) {
+	t.Fail()
 }
 
-// func TestGetUser2(t *testing.T) {
-// 	username := "invalid-user"
-// 	if resp := getUserTest(username, t); resp != "" {
-// 		t.Errorf("got invalid response, expected %v, got: %v", username, resp)
-// 	}
-// }
-
-// func TestCreateUser(t *testing.T) {
-// 	createUserTest("test-user", t)
-// }
-
-// func TestDeleteUser1(t *testing.T) {
-// 	if resp := getUserTest("test-user", t); resp == "test-user" {
-// 		deleteUserTest("test-user", t)
-// 	} else {
-// 		createUserTest("test-user", t)
-// 		deleteUserTest("test-user", t)
-// 	}
-// }
-
-// func TestDeleteUser2(t *testing.T) {
-// 	if resp := getUserTest("test-user", t); resp == "test-user" {
-// 		deleteUserTest("test-user", t)
-// 		deleteUserTest("test-user", t)
-// 	} else {
-// 		deleteUserTest("test-user", t)
-// 	}
-// }
-
-// func TestUpdateUser1(t *testing.T) {
-// 	if resp := getUserTest("test-user", t); resp == "test-user" {
-// 		deleteUserTest("test-user", t)
-// 	}
-// 	createUserTest("test-user", t)
-// 	updateUserTest("test-user", "new-password", t)
-// 	deleteUserTest("test-user", t)
-
-// }
-
-// func TestUpdateUser2(t *testing.T) {
-// 	deleteUserTest("test-user", t)
-// 	updateUserTest("test-user", "new-password", t)
-// }
+func TestPasswordDecoding(t *testing.T) {
+	t.Fail()
+}
