a6c2f1e8155047977a99d3f8d4834851bb38fc64
ramawama <93228765+ramawama@users.noreply.github.com>
2023-03-29 15:57:05 -0400

    removed superfluous statements

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -3,9 +3,6 @@ import { Loader } from "@googlemaps/js-api-loader";
 import { Component } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
-// const { StreetViewPreference } = (await google.maps.importLibrary(
-//   "streetView"
-// )) as google.maps.StreetViewLibrary;
 
 @Component({
   selector: "app-landing-page",
@@ -19,29 +16,6 @@ export class LandingPageComponent {
   long = this.randomLong();
   invalidLoc: boolean = true;
 
-  // content: string;
-  // private _jsonURL = "src/assets/env.json";
-  // constructor(private http: HttpClient) {
-  //   this.getJSON().subscribe((data) => {
-  //     console.log(data);
-  //   });
-  // }
-  // public getJSON(): Observable<any> {
-  //   return this.http.get(this._jsonURL);
-  // }
-
-  // getTextFile() {
-  //   this.http
-  //     .get("/env.txt", {
-  //       responseType: "text",
-  //     })
-  //     .subscribe((data) => {
-  //       this.content = data;
-  //       //console.log(this.content);
-  //    });
-  // }
-
-  // center: LatLngLiteral = { lat: 29.6516, lng: -82.3248 };
   randomLat() {
     return Math.random() * (29.769872 - 29.602758) + 29.769872;
   }
@@ -95,22 +69,6 @@ export class LandingPageComponent {
         }
       };
       RandomLoc(HandlePanoramaData);
-      // var cr = new google.maps.LatLng(this.lat, this.long);
-      // //streetView.getPanorama({ location: cr, radius: 50 });
-      // var sStatus = new google.maps.StreetViewService();
-
-      // sStatus.getPanorama({ location: cr, radius: 1000 }, (data, status) => {
-      //   if (status === "OK") {
-      //     console.log("valid panorama");
-      //   } else {
-      //     console.log("invalid panorama");
-
-      //     this.invalidLoc = true;
-      //     this.lat = this.randomLat();
-      //     this.long = this.randomLong();
-      //     cr = new google.maps.LatLng(this.lat, this.long);
-      //   }
-      // });
 
       new google.maps.Map(document.getElementById("Gmap") as HTMLElement, {
         center: { lat: 29.653288, lng: -82.338712 },
@@ -128,6 +86,5 @@ export class LandingPageComponent {
         },
       });
     });
-    //StreetViewService().getPanorama
   }
 }
