5729981f5ad1cfe8c39970b5ea48a1532dfaa559
Coomesicle <stephencoomes@ufl.edu>
2023-03-29 13:06:47 -0400

    :angry: Some theming works

--- src/app/components/banner/banner.component.ts
+++ src/app/components/banner/banner.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { OverlayContainer } from "@angular/cdk/overlay";
 
 @Component({
   selector: "app-banner",
@@ -6,20 +7,23 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./banner.component.scss"],
 })
 export class BannerComponent implements OnInit {
-  selectedValue = "Light";
+  selectedValue = true;
   selectedTheme = "light_mode";
 
+  constructor(private overlay: OverlayContainer) {}
   ngOnInit() {
     this.selectedTheme = localStorage.getItem("selectedTheme") || "light_mode";
-    this.selectedValue = this.selectedTheme === "light_mode" ? "Light" : "Dark";
+    this.selectedValue = this.selectedTheme === "light_mode" ? true : false;
   }
 
   toggleTheme() {
-    if (this.selectedValue === "Light") {
-      this.selectedValue = "Dark";
+    if (this.selectedValue) {
+      this.overlay.getContainerElement().classList.add("dark_mode");
+      this.selectedValue = false;
       this.selectedTheme = "dark_mode";
     } else {
-      this.selectedValue = "Light";
+      this.overlay.getContainerElement().classList.remove("dark_mode");
+      this.selectedValue = true;
       this.selectedTheme = "light_mode";
     }
     localStorage.setItem("selectedTheme", this.selectedTheme);
