6393612246230d89e4722dbc6c1f2b17b206fbae
ramawama <93228765+ramawama@users.noreply.github.com>
2023-04-12 20:08:14 -0400

    OOPS
    I published my API key, i have no changed it

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -11,7 +11,7 @@ import { Observable } from "rxjs";
 })
 export class LandingPageComponent {
   title = "GatorMap";
-  string = "AIzaSyArV4qnzQ40J1nt6FIPYIYqwSYrOKztUzo";
+  string = "";
   lat = this.randomLat();
   long = this.randomLong();
   invalidLoc: boolean = true;
