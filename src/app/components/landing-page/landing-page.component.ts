aeff29697e0664aa62fa1089380c918db6b26bfb
ramawama <93228765+ramawama@users.noreply.github.com>
2023-04-17 16:09:47 -0400

    Added marker functions
    Need to limit user to one

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -72,22 +72,39 @@ export class LandingPageComponent {
       };
       RandomLoc(HandlePanoramaData);
 
-      new google.maps.Map(document.getElementById("Gmap") as HTMLElement, {
-        center: { lat: 29.653288, lng: -82.338712 },
-        zoom: 11,
-        disableDefaultUI: true,
+      const navMap = new google.maps.Map(
+        document.getElementById("Gmap") as HTMLElement,
+        {
+          center: { lat: 29.653288, lng: -82.338712 },
+          zoom: 11,
+          disableDefaultUI: false,
+          mapTypeControl: false,
 
-        // restriction: {
-        //   latLngBounds: {
-        //     //North: nw 53rd ave
-        //     east: -82.295573,
-        //     north: 29.676191,
-        //     south: 29.616823,
-        //     west: -82.398928,
-        //   },
-        //   strictBounds: false,
-        // },
-      });
+          // restriction: {
+          //   latLngBounds: {
+          //     //North: nw 53rd ave
+          //     east: -82.295573,
+          //     north: 29.676191,
+          //     south: 29.616823,
+          //     west: -82.398928,
+          //   },
+          //   strictBounds: false,
+          // },
+        }
+      );
+      google.maps.event.addListener(
+        navMap,
+        "click",
+        function (e: { latLng: google.maps.LatLng }) {
+          placeMarker(e.latLng, navMap);
+        }
+      );
+      function placeMarker(Location: google.maps.LatLng, Map: google.maps.Map) {
+        var marker = new google.maps.Marker({
+          position: Location,
+          map: navMap,
+        });
+      }
     });
   }
 }
