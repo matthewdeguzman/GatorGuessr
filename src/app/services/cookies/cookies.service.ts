2179e7525558e74e9e731c19933ad9a20882c13f
Coomesicle <stephencoomes@ufl.edu>
2023-04-17 19:13:43 -0400

    :cookie: cookie

--- src/app/services/cookies/cookies.service.ts
+++ src/app/services/cookies/cookies.service.ts
@@ -1,5 +1,10 @@
 import { Injectable } from "@angular/core";
-import { HttpClient, HttpParams } from "@angular/common/http";
+import {
+  HttpClient,
+  HttpHeaders,
+  HTTP_INTERCEPTORS,
+  HttpParams,
+} from "@angular/common/http";
 import { map } from "rxjs/operators";
 import { CookieService } from "ngx-cookie-service";
 
@@ -12,14 +17,16 @@ export class CookiesService {
 
   // Gets cookie
   getCookie(username: string) {
+    document.cookie = `key=Cookie; path=http://localhost:9000/cookies/set/${username}/`;
+    const headers = new HttpHeaders().set(document.cookie, username);
     return this.http
       .get(`${this.uri}/cookies/set/${username}/`, {
+        headers: headers,
         observe: "response",
+        withCredentials: true,
         responseType: "text",
       })
-      .subscribe((res) => {
-        console.log(res.body);
-      });
+      .pipe(map((response) => response.status));
   }
   // Checks if cookie is valid
   validateCookie() {
