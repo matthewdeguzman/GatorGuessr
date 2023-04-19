8d36753d137248ec150c5e88f037f476ac7e5a94
Ethan <efan@ufl.edu>
2023-04-19 18:02:05 -0400

    new test

--- src/app/components/account/account.component.ts
+++ src/app/components/account/account.component.ts
@@ -1,7 +1,7 @@
 import { Component } from "@angular/core";
 import { MatDialogRef } from "@angular/material/dialog";
 import { IssueService } from "src/app/services/issue.service";
-import { FormGroup, FormControl } from "@angular/forms";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
   selector: "app-account",
@@ -13,12 +13,26 @@ export class AccountComponent {
   newPassword: string;
   newUsername: string;
   public showPassword: boolean = false;
+  public showSubmit: boolean = false;
+  public invalidUser: boolean = false;
 
   updateForm = new FormGroup({
     newUsername: new FormControl(""),
     newPassword: new FormControl(""),
   });
-
+  usernameRequirements = new FormControl("", [
+    Validators.required,
+    Validators.minLength(4),
+    Validators.maxLength(20),
+  ]);
+  passwordRequirements = new FormControl("", [
+    Validators.required,
+    Validators.minLength(8),
+    Validators.maxLength(25),
+    Validators.pattern(
+      "^(?=[^A-Z]*[A-Z])(?=[^a-z]*[a-z])(?=\\D*\\d)[A-Za-z\\d!$%@#£€*?&]{8,}$"
+    ),
+  ]);
   constructor(
     private IssueService: IssueService,
     private dialogRef: MatDialogRef<AccountComponent>
@@ -33,7 +47,14 @@ export class AccountComponent {
     this.IssueService.updateUser(newUsername, newPassword).subscribe((res) => {
       console.log(res);
     });
-
     this.closeDialog();
+  }
+  verifySubmit(): boolean {
+    if (this.usernameRequirements.valid && this.passwordRequirements.valid) {
+      return false; //means it will display
+    } else return true;
+  }
+  public showUserError(): boolean {
+    return this.invalidUser;
   }
 }
