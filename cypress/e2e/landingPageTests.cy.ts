ee8d9d8895115c31e3cc4a8d66d971eec9216e14
Ethan <efan@ufl.edu>
2023-04-19 18:44:39 -0400

    fixed cypress tests

--- cypress/e2e/landingPageTests.cy.ts
+++ cypress/e2e/landingPageTests.cy.ts
@@ -1,8 +1,8 @@
 describe("Clicks on the map", () => {
     beforeEach(() => {
         cy.visit("http://localhost:4200/login");
-        cy.get("[id^=username]").type("Frontend");
-        cy.get("[id^=password]").type("FrontendIsNumber1");
+        cy.get("[id^=username]").type("testuser1");
+        cy.get("[id^=password]").type("Password123");
         cy.get("[id^=login-button]").click();
     })
     it("Clicks on the map and clicks submit", () => {
@@ -13,8 +13,8 @@ describe("Clicks on the map", () => {
 describe("Signs out", () => {
     beforeEach(() => {
         cy.visit("http://localhost:4200/login");
-        cy.get("[id^=username]").type("Frontend");
-        cy.get("[id^=password]").type("FrontendIsNumber1");
+        cy.get("[id^=username]").type("testuser1");
+        cy.get("[id^=password]").type("Password123");
         cy.get("[id^=login-button]").click();
     })
     it("Clicks on the signout button", () => {
@@ -44,18 +44,18 @@ describe("Test delete user", () => {
         cy.get("[id^=login-button]").click();
         cy.get("div").contains("Error: No User Found");
     });
-    describe("Test the next button", () => {
-        beforeEach(() => {
-            cy.visit("http://localhost:4200/login");
-            cy.get("[id^=username]").type("Frontend");
-            cy.get("[id^=password]").type("FrontendIsNumber1");
-            cy.get("[id^=login-button]").click();
-        })
-        it("Clicks on the map, submits and clicks next", () => {
-            cy.get("[id^=Gmap]").click("center");
-            cy.get("[id^=submitButton]").click();
-            cy.get("[id^=nextButton]").click();
-            cy.url().should("eq", "http://localhost:4200/landing-page");
-        });
+});
+describe("Test the next button", () => {
+    beforeEach(() => {
+        cy.visit("http://localhost:4200/login");
+        cy.get("[id^=username]").type("testuser1");
+        cy.get("[id^=password]").type("Password123");
+        cy.get("[id^=login-button]").click();
+    })
+    it("Clicks on the map, submits and clicks next", () => {
+        cy.get("[id^=Gmap]").click("center");
+        cy.get("[id^=submitButton]").click();
+        cy.get("[id^=nextButton]").click();
+        cy.url().should("eq", "http://localhost:4200/landing-page");
     });
-  });
+});
